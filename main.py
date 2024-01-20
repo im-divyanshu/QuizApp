ec68377c8036dbab1ddfb178ac80c1d6ce832f3e
Divyanshu Singh <85323719+im-divyanshu@users.noreply.github.com>
2024-01-20 14:26:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -45,38 +45,38 @@ class SecondWindow(Screen):
 class WindowManager(ScreenManager):
     pass
 
-# class MainWidget(Widget):
-#     quiz_json=requests.get('https://the-trivia-api.com/api/questions').json()
-#     count=0
-#     question=StringProperty(quiz_json[0]['question'])
-#     print(question)
-#     options=quiz_json[0]['incorrectAnswers']
-#     options.append(quiz_json[0]['correctAnswer'])
-#     print(quiz_json[0]['correctAnswer'])
-#     random.shuffle(options)
-#     b0text=StringProperty(options[0])
-#     b1text=StringProperty(options[1])
-#     b2text=StringProperty(options[2])
-#     b3text=StringProperty(options[3])
+class MainWidget(Widget):
+    quiz_json=requests.get('https://the-trivia-api.com/api/questions').json()
+    count=0
+    question=StringProperty(quiz_json[0]['question'])
+    print(question)
+    options=quiz_json[0]['incorrectAnswers']
+    options.append(quiz_json[0]['correctAnswer'])
+    print(quiz_json[0]['correctAnswer'])
+    random.shuffle(options)
+    b0text=StringProperty(options[0])
+    b1text=StringProperty(options[1])
+    b2text=StringProperty(options[2])
+    b3text=StringProperty(options[3])
 
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#     def update(self,widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    def update(self,widget):
 
-#         if(widget.text==self.quiz_json[self.count]['correctAnswer']):
-#             self.count+=1
-#             self.question=self.quiz_json[self.count]['question']
-#             options=self.quiz_json[self.count]['incorrectAnswers']
-#             options.append(self.quiz_json[self.count]['correctAnswer'])
-#             print(self.quiz_json[self.count]['correctAnswer'])
-#             random.shuffle(options)
-#             self.b0text=options[0]
-#             self.b1text=options[1]
-#             self.b2text=options[2]
-#             self.b3text=options[3]
-#         if self.count==9:
-#             self.count=0
-#             self.quiz_json=requests.get('https://the-trivia-api.com/api/questions').json()
+        if(widget.text==self.quiz_json[self.count]['correctAnswer']):
+            self.count+=1
+            self.question=self.quiz_json[self.count]['question']
+            options=self.quiz_json[self.count]['incorrectAnswers']
+            options.append(self.quiz_json[self.count]['correctAnswer'])
+            print(self.quiz_json[self.count]['correctAnswer'])
+            random.shuffle(options)
+            self.b0text=options[0]
+            self.b1text=options[1]
+            self.b2text=options[2]
+            self.b3text=options[3]
+        if self.count==9:
+            self.count=0
+            self.quiz_json=requests.get('https://the-trivia-api.com/api/questions').json()
 
 kv=Builder.load_file("app.kv")
 
